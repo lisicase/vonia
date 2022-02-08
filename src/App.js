248924d2e1c6d2d8a66d42e58bc2dec55a804506
lisicase <casee@uw.edu>
2022-02-08 13:58:15 -0800

    added different pages and nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <TestNavigationBar />
+        <Routes>
+          <Route path='/' element={<p>Home</p>} />
+          <Route path='/bathroom' element={<p>Bathroom</p>} />
+          <Route path='/reviews' element={<p>Reviews</p>} />
+          <Route path='/signin' element={<p>Sign In</p>} />
+        </Routes>
+      </>
     </div>
   );
+}
+
+class TestNavigationBar extends React.Component {
+  render() {
+      return (
+          <header>
+              <div className="header-div">
+                  <Navbar expand="lg">
+                      <Navbar.Brand><NavLink exact to='/' className="navLink">Spotty</NavLink></Navbar.Brand>
+                      {/*<Navbar.Toggle aria-controls="basic-navbar-nav" />*/}
+                      <Navbar.Collapse id="basic-navbar-nav">
+                          <Nav className="mr-auto">
+                              <Nav.Link><NavLink exact to='/' className="navLink" >Home</NavLink></Nav.Link>
+                              <Nav.Link><NavLink exact to='/bathroom' className="navLink" >Bathroom</NavLink></Nav.Link>
+                              <Nav.Link><NavLink exact to='/reviews' className="navLink">Review History</NavLink></Nav.Link>
+                              <Nav.Link><NavLink exact to='/signin' className="navLink">Sign In</NavLink></Nav.Link>
+                          </Nav>
+                      </Navbar.Collapse>
+                  </Navbar>
+              </div>
+          </header>
+      )
+  }
 }
 
 export default App;
