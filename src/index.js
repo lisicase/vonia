040651800e341c8a35f45452614dbe3bdfd577ae
Kevin Chou <kchou0907@gmail.com>
2022-02-24 00:11:56 -0800

    revert changes

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import './index.css';
 import App from './App';
 //firebase
 
+import 'firebase/auth';
+import 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
