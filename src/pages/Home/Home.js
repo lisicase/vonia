8ea051a0ab40116c6eb73a1b92f84e689df1013a
Kevin Chou <kchou0907@gmail.com>
2022-02-28 23:04:41 -0800

    implement split map

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -12,9 +12,23 @@ export default function HomePage() {
     const map = useRef(null);
     const [lng, setLng] = useState(-122.3035);
     const [lat, setLat] = useState(47.655548);
-    const [zoom, setZoom] = useState(15);
+    const [zoom, setZoom] = useState(13);
+
+    /** geolocation is a bit scuffed atm
+    if ("geolocation" in navigator) {
+        console.log("Geolocation available");
+        navigator.geolocation.getCurrentPosition((position) => {
+            console.log(`geo lng: ${position.coords.longitude} lat: ${position.coords.latitude}`);
+            setLat(position.coords.latitude);
+            setLng(position.coords.longitude);
+        });
+    } else {
+        console.log("Geolocation not available");
+    }
+    */
 
     useEffect(() => {
+        console.log(`lng: ${lng} lat: ${lat}`);
         if (map.current) return; // initialize map only once
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
@@ -27,24 +41,28 @@ export default function HomePage() {
     useEffect(() => {
         if (!map.current) return; // wait for map to initialize
         map.current.on('move', () => {
-          setLng(map.current.getCenter().lng.toFixed(4));
-          setLat(map.current.getCenter().lat.toFixed(4));
-          setZoom(map.current.getZoom().toFixed(2));
+            setLng(map.current.getCenter().lng.toFixed(4));
+            setLat(map.current.getCenter().lat.toFixed(4));
+            setZoom(map.current.getZoom().toFixed(2));
         });
-      });
-      
+    });
+
     return (
         <div>
             <h1>Spotty</h1>
             <h2>Spot-a-Potty</h2>
             <form action="" className="mt-3">
-                <input type="text" placeholder="Location" />
-                <div>
-                    <div ref={mapContainer} className="map-container" />
+                <div class='sidebar'>
+                    <div class='addressBar'>
+                        <input type="text" placeholder="Location" />
+                        <Button variant="primary btn-sm" id="search-button" type="submit">
+                            <i className="fa fa-search" aria-hidden="true"></i>
+                        </Button>{' '}
+                    </div>
+                    <div id='listings' class='listings'>
+                    </div>
                 </div>
-                <Button variant="primary btn-sm" id="search-button" type="submit">
-                    <i className="fa fa-search" aria-hidden="true"></i>
-                </Button>{' '}
+                <div ref={mapContainer} className="map-container" />
             </form>
         </div>
     );
