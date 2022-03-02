8c5fe718ecae3617eb9a5701f7200228fb12bcf7
lisicase <casee@uw.edu>
2022-03-02 09:17:55 -0800

    added images and arrows to gallery/features

--- src/BathroomInfo/BathroomFeatures.js
+++ src/BathroomInfo/BathroomFeatures.js
@@ -3,18 +3,23 @@ import React from 'react';
 import { Component } from 'react';
 // Icons
 import { AiOutlineCheckCircle } from "react-icons/ai";
-// Style
+import { BsChevronCompactRight } from "react-icons/bs";
 
 export class BathroomFeatures extends Component {
     render() {
         return (
             <div>
                 <h2>Features</h2>
-                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                    <SingleBathroomFeature feature="Tall Stalls" />
-                    <SingleBathroomFeature feature="Paper Towels" />
-                    <SingleBathroomFeature feature="Auto Flush" />
-                    <SingleBathroomFeature feature="Hand Dryer" />
+                <div style={{display:'flex', justifyContent:'space-between'}}>
+                    <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                        <SingleBathroomFeature feature="Tall Stalls" />
+                        <SingleBathroomFeature feature="Paper Towels" />
+                        <SingleBathroomFeature feature="Auto Flush" />
+                        <SingleBathroomFeature feature="Hand Dryer" />
+                    </div>
+                    <div style={{display:'flex', alignItems:'center'}}>
+                        <BsChevronCompactRight size={20} />
+                    </div>
                 </div>
             </div>
         );
