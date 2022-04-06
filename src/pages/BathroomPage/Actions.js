e6fa2805ce0100bee643bc8754d97c4c38d324e1
lisicase <casee@uw.edu>
2022-04-05 23:43:54 -0700

    turned components into functions

--- src/pages/BathroomPage/Actions.js
+++ src/pages/BathroomPage/Actions.js
@@ -5,28 +5,24 @@ import { FiShare2 } from "react-icons/fi";
 import { HiOutlinePencilAlt } from "react-icons/hi";
 import { VscHeart } from "react-icons/vsc";
 
-export class BathroomActions extends Component {
-    render() {
-        return (
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-                <ActionWithTitle icon={<BsMap size={30} />} title="Directions" />
-                <ActionWithTitle icon={<VscHeart size={30} />} title="Favorite" />
-                <ActionWithTitle icon={<HiOutlinePencilAlt size={30} />} title="Review" />
-                <ActionWithTitle icon={<FiShare2 size={30} />} title="Share" />
-            </div>
-        );
-    }
+export function BathroomActions() {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+            <ActionWithTitle icon={<BsMap size={30} />} title="Directions" />
+            <ActionWithTitle icon={<VscHeart size={30} />} title="Favorite" />
+            <ActionWithTitle icon={<HiOutlinePencilAlt size={30} />} title="Review" />
+            <ActionWithTitle icon={<FiShare2 size={30} />} title="Share" />
+        </div>
+    );
 }
 
-class ActionWithTitle extends Component {
-    render() {
-        return (
-            <div className="textCenter" >
-                <div className="actionBtn">
-                    {this.props.icon}
-                </div>
-                <p>{this.props.title}</p>
+function ActionWithTitle({icon, title}) {
+    return (
+        <div className="textCenter" >
+            <div className="actionBtn">
+                {icon}
             </div>
-        );
-    }
+            <p>{title}</p>
+        </div>
+    );
 }
