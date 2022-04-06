3460b48b9217c9a059ee44085d6a5bcc7765eb3e
lisicase <casee@uw.edu>
2022-04-06 00:01:54 -0700

    implemented favoriting ui

--- src/pages/BathroomPage/Actions.js
+++ src/pages/BathroomPage/Actions.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Component } from 'react';
-import { BsMap } from "react-icons/bs";
-import { FiShare2 } from "react-icons/fi";
+//React
+import React, { useState } from 'react';
+// Icons
+import { FiShare2, FiMap } from "react-icons/fi";
 import { HiOutlinePencilAlt } from "react-icons/hi";
-import { VscHeart } from "react-icons/vsc";
+import { FaRegHeart, FaHeart } from "react-icons/fa";
 
 export function BathroomActions() {
+    const [favorited, toggleFavorited] = useState(false);
+
+    let favBtn = <div onClick={() => toggleFavorited(true)}>
+                    <ActionWithTitle icon={<FaRegHeart size={30} />} title="Favorite" />
+                </div>;
+    if (favorited) {
+        favBtn = <div onClick={() => toggleFavorited(false)}>
+                    <ActionWithTitle icon={<FaHeart size={30} />} title="Favorite" />
+                </div>;
+    }
+
     return (
         <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
-            <ActionWithTitle icon={<BsMap size={30} />} title="Directions" />
-            <ActionWithTitle icon={<VscHeart size={30} />} title="Favorite" />
+            <ActionWithTitle icon={<FiMap size={30} />} title="Directions" />
+            {favBtn}
             <ActionWithTitle icon={<HiOutlinePencilAlt size={30} />} title="Review" />
             <ActionWithTitle icon={<FiShare2 size={30} />} title="Share" />
         </div>
