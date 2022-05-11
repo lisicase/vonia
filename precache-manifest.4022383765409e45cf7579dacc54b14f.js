ba69f6fb5ccf0d81f99d084da943c68f7fd4de4a
lisicase <casee@uw.edu>
2022-05-11 10:55:59 -0700

    Updates

---
+++ precache-manifest.4022383765409e45cf7579dacc54b14f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4bd66837d7038fc4694",
+    "url": "/vonia/static/css/main.1950da10.chunk.css"
+  },
+  {
+    "revision": "f4bd66837d7038fc4694",
+    "url": "/vonia/static/js/main.6e056855.chunk.js"
+  },
+  {
+    "revision": "cfd69c213908142c0a7b",
+    "url": "/vonia/static/js/runtime~main.aef5538c.js"
+  },
+  {
+    "revision": "8ca5adec09376cef1ffd",
+    "url": "/vonia/static/css/2.0378ee88.chunk.css"
+  },
+  {
+    "revision": "8ca5adec09376cef1ffd",
+    "url": "/vonia/static/js/2.5eb37449.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vonia/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vonia/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vonia/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vonia/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vonia/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "d26c691ee7a39be1ed0afea11638ef5b",
+    "url": "/vonia/index.html"
+  }
+];
